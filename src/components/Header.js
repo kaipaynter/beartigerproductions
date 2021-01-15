f2d02eb74c7125c90ac88d0daddf6c62829c3651
boipelomawashada <boipelo.mawasha@directaxis.co.za>
2021-01-15 08:09:45 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,79 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import mainIcon from '../images/mainIcon.jpeg'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo"> 
+        <img className="logo" src={mainIcon} alt="" />
+    </div>
+    
+    <div className="content">
+      <div className="inner">
+        <h1>BearTiger Productions</h1>
+        <p>
+          Founded in 2015 BearTiger Productions is an artist-driven production
+          company that develops and produces music, film, theatre, television,
+          radio, and digital content. BearTiger Productions has a dedicated team
+          of professionals from across the world giving it an international
+          flair and buzzing energy…Someplace, somewhere it’s 7 am!
+          <br />
+          <br />
+          From American writers and producers to Australian and UK execs, the
+          team prides itself on giving you something a little different from the
+          “norm”.
+          <br />
+          <br />
+          BearTiger Productions is adventurous, promising, and real.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('missionstatement')
+            }}
+          >
+            Mission Statement
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('thecrew')
+            }}
+          >
+            The Crew
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('projects')
+            }}
+          >
+            Projects
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,179 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import mission from '../images/mission.jpg'
+import crew from '../images/crew.jpg'
+import clapperboard from '../images/clapperboard.jpg'
+import wannacoffeelogo from '../images/wannacoffeelogo.jpg'
+
+class Main extends React.Component {
+  render() {
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      ></div>
+    )
+
+    return (
+      <div
+        ref={this.props.setWrapperRef}
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="missionstatement"
+          className={`${
+            this.props.article === 'missionstatement' ? 'active' : ''
+          } ${this.props.articleTimeout ? 'timeout' : ''}`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Mission Statement</h2>
+          <span className="image main">
+            <img src={mission} alt="" />
+          </span>
+          <p>
+            BearTiger Productions is committed to cultivating content that is
+            diverse, fresh, and relatable. Content, that not only entertains but
+            inspires. Using a range of mediums, for its creative expression,
+            their aim is to tell the greatest story and share it across the
+            globe. Working with talent who range from well known to unknown,
+            seasoned to novice but mostly, creatives who are on the pulse and
+            cutting edge of their craft. They are impassioned storytellers, who
+            unapologetically bring a truthful reflection of the world around
+            them, give a platform to the voices who have yet to be heard, and
+            dare to dream whilst doing it.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="thecrew"
+          className={`${this.props.article === 'thecrew' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">The Crew</h2>
+          <span className="image main">
+            <img src={crew} alt="" />
+          </span>
+          <p>
+            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
+            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
+            urna nisi, fringila lorem et vehicula lacinia quam. Integer
+            sollicitudin mauris nec lorem luctus ultrices.
+          </p>
+          <p>
+            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
+            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
+            Pellentesque condimentum sem. In efficitur ligula tate urna.
+            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
+            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
+            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
+            tempus.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="projects"
+          className={`${this.props.article === 'projects' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Projects</h2>
+          <p>For all project info, click for details...</p>
+          <span className="image main">
+            <a href="https://www.instagram.com/beartiger.productions/">
+              <img src={clapperboard} alt="" />
+            </a>
+          </span>
+          <p>
+            Wanna Coffee? Podcast
+            <br /> 
+            Thursdays at 10am AEST/Wednesdays at 8pm EST
+          </p>
+          <span className="image main">
+            <a href="https://www.youtube.com/playlist?list=PLzXFzLvgInhiSfY4-r8PVP2wZNKcgMx4m">
+              <img src={wannacoffeelogo} alt="" />
+            </a>
+          </span>
+          {close}
+        </article>
+
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Contact</h2>
+          <form method="post" action="https://formspree.io/f/xjvpjdvb">
+            <div className="field half first">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="_replyto" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+            <ul className="actions">
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
+            </ul>
+          </form>
+          <ul className="icons">
+            <li>
+              <a
+                href="https://twitter.com/BearTigerInfo"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/beartigerproductions"
+                className="icon fa-facebook"
+              >
+                <span className="label">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/beartiger.productions"
+                className="icon fa-instagram"
+              >
+                <span className="label">Instagram</span>
+              </a>
+            </li>
+          </ul>
+          {close}
+        </article>
+      </div>
+    )
+  }
+}
+
+Main.propTypes = {
+  route: PropTypes.object,
+  article: PropTypes.string,
+  articleTimeout: PropTypes.bool,
+  onCloseArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+  setWrapperRef: PropTypes.func.isRequired,
+}
+
+export default Main
