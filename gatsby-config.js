a1f2b403eb921d649caff1fc3bae033fc86b842a
beepa <admin@beartigerproductions.com>
2021-09-16 07:53:19 +0200

    config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,5 +50,11 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/assets/images`
+      }
+    },
   ],
 }
